54dda3f55c90934d05085f62607a59295e149130
Izakami <87597619+Izakami0x@users.noreply.github.com>
2025-08-24 11:54:29 +0800

    Update: added operators.js
    Functions that add/subtract/multiply,divide 2 numbers

---
+++ javascript/operators.js
@@ -0,0 +1,35 @@
+function sum(firstNumber,secondNumber) {
+  console.log(firstNumber + secondNumber);
+  return firstNumber + secondNumber;
+}
+function difference(firstNumber,secondNumber) {
+  console.log(firstNumber - secondNumber);
+  return firstNumber - secondNumber;
+}
+function product(firstNumber,secondNumber) {
+  console.log(firstNumber * secondNumber);
+  return firstNumber * secondNumber;
+}
+function quotient(firstNumber,secondNumber) {
+  console.log(firstNumber / secondNumber);
+  return firstNumber / secondNumber;
+}
+
+function calculate (firstNumber, operator, secondNumber){
+switch (operator) {
+  case "+" :
+    sum (firstNumber,secondNumber);
+    break;
+  case "-" :
+    difference (firstNumber,secondNumber);
+    break;
+  case "*" :
+    product (firstNumber,secondNumber);
+    break;
+  case "/" :
+    quotient (firstNumber,secondNumber);
+    break;
+}
+}
+calculate(1,"/",1)
+
